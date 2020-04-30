00004fa7cd3919272fe4dea4a2862decddb4f8ca
Abhishek Gupta <agabhishek20121998@gmail.com>
2020-04-30 16:35:44 +0530

    initial commit

---
+++ platformspecs.py
@@ -0,0 +1,11 @@
+# importing platform module
+import platform
+
+# printing platform name
+print("Platform Name : ", platform.platform())
+# printing system name
+print("System Name : ",platform.system())
+# printing processor details
+print("Processor Name : ",platform.processor())
+# printing architecture of the system
+print("Architecture : ",platform.architecture())
